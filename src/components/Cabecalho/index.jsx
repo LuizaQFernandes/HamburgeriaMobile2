6f54167cffed23332b52fa5f8860ad3ee16bc6bc
LuizaQFernandes <luhqfernandes@gmail.com>
2022-03-07 21:08:54 -0300

    Animations Added

--- src/components/Cabecalho/index.jsx
+++ src/components/Cabecalho/index.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { View, Text } from 'react-native';
-
+import { MotiView} from 'moti'
 import { styles } from './styles';
 import { theme } from '../../styles/theme';
 
 export function Cabecalho() {
   return (
-    <View style={styles.container}>
+    <MotiView 
+     
+      from={{
+        opacity: 0,
+        scale: 2.5
+      }}
+      animate={{
+        opacity: 1,
+        scale: 1
+      }}
+      transition={{
+        type: 'timing',
+        duration: 3000
+      }}
+    
+    style={styles.container}>
+
       <Feather
         name="arrow-left"
         color={theme.colors.brown}
@@ -23,6 +39,6 @@ export function Cabecalho() {
           Hamburgueria
         </Text>
       </View>
-    </View>
+    </MotiView>
   );
 }
